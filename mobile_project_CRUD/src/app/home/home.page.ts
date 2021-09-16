704a378e8881db9e5427ac765317f014ee4b9366
Adrián Mora <Adri98.1mm@gmail.com>
2021-09-16 13:24:13 -0600

    actualizacion

--- mobile_project_CRUD/src/app/home/home.page.ts
+++ mobile_project_CRUD/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  ProductForm: FormGroup;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {
+    this.ProductForm = this.fb.group({
+      codigo: [null, [Validators.required, Validators.minLength(1)]],
+      nombre: [null, [Validators.required, Validators.minLength(1)]],
+      cantidad: [null, [Validators.required, Validators.minLength(1)]]
+    });
+  }
+
+  numberOnlyValidation(event: any) {
+    let pattern = /[0-9]/;
+    let inputChar = String.fromCharCode(event.charCode);
+    let cant: number = this.ProductForm.value.cantidad;
+    if(cant == null || cant == 0)
+      pattern = /[1-9]/;
+
+    if (!pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
 
 }
